09ec1f38cef041c17bdb4387236da2b807b60fc4
oo-00 <29607330+oo-00@users.noreply.github.com>
2025-12-25 11:19:55 -0500

    Events, comments, flow

--- contracts/bachvanity.sol
+++ contracts/bachvanity.sol
@@ -22,25 +22,33 @@ contract Bachvanity {
     mapping(address => uint256[]) public userOrderIDs;
 
     event Deployed(address addr, bytes32 salt);
+    event OrderCreated(uint256 orderId, address requestOwner, uint256 amount, bytes32 bytecodeHash, address deployer, string addressPattern, uint160 deadline);
+    event SaltSubmitted(uint256 orderId, address submitter, bytes32 salt, uint8 points);
+    event OrderClosed(uint256 orderId, address winner, uint256 amount);
 
+    // View functions
+
+    /*
+    * @return total number of orders
+    */
     function ordersLength() public view returns (uint256) {
         return orders.length;
     }
+
+    /*
+    * @return number of orders created by user
+    */
     function userOrderIDsLength(address user) public view returns (uint256) {
         return userOrderIDs[user].length;
     }
 
-    function deploy(bytes memory code, bytes32 salt) public {
-        address addr;
-        assembly {
-            addr := create2(0, add(code, 0x20), mload(code), salt)
-            if iszero(extcodesize(addr)) {
-                revert(0, 0)
-            }
-        }
-        emit Deployed(addr, salt);
-    }
-
+    /*
+    * @dev compute address for given salt, bytecodeHash and deployer
+    * @param salt used for address computation
+    * @param bytecodeHash keccak256 hash of the contract bytecode (INCLUDING constructor args) to be deployed 
+    * @param create2 factory that will deploy the contract
+    * @return computed address for given salt, bytecodeHash and deployer
+    */
     function computeAddress(bytes32 salt, bytes32 bytecodeHash, address deployer) public pure returns (address addr) {
         assembly ("memory-safe") {
             let ptr := mload(0x40)
@@ -53,6 +61,36 @@ contract Bachvanity {
         }
     }
 
+    /*
+    * @dev simulate points for given salt in order
+    * @param orderId id of the order
+    * @param salt salt to simulate
+    * @return points for given salt in order
+    */
+    function simPoints(uint256 orderId, bytes32 salt) public view returns(uint8) {
+        Orders memory order = orders[orderId];
+        bytes32 bytecodeHash = order.bytecodeHash;
+        address deployer = order.deployer;
+        address addr = computeAddress(salt, bytecodeHash, deployer);
+        bytes memory addrBytes = bytes(Strings.toHexString(uint256(uint160(addr)), 20));
+        uint8 points = 0;
+        for(uint256 i = 2; i < addrBytes.length; i++) {
+            if(order.desired[i - 2] == addrBytes[i]) {
+                points++;
+            }
+        }
+        return points;
+    }
+
+    // Write functions
+
+    /*
+    * @dev create a vanity address request
+    * @param deployer address that will deploy the contract, if address(0) then this contract will deploy
+    * @param bytecodeHash keccak256 hash of the contract bytecode to be deployed
+    * @param addressPattern desired address pattern as hex string, use 'X' for wildcard characters, e.g. 'deadbeefXXXXXX...'
+    * @param deadline timestamp until which the request is valid
+    */
     function createRequest(address deployer, bytes32 bytecodeHash, string memory addressPattern, uint160 deadline) public payable {
         require(msg.value > 0, "!value");
         require(deadline > block.timestamp, "!deadline");
@@ -86,8 +124,14 @@ contract Bachvanity {
             bestSubmitter: address(0)
         }));
         userOrderIDs[msg.sender].push(orders.length - 1);
+        emit OrderCreated(orders.length - 1, msg.sender, msg.value, bytecodeHash, deployer, addressPattern, deadline);
     }
 
+    /*
+    * @dev submit a salt for given order
+    * @param orderId id of the order
+    * @param salt salt to submit
+    */
     function submitSalt(uint256 orderId, bytes32 salt) public {
         Orders storage order = orders[orderId];
         require(block.timestamp <= order.deadline, "!deadline");
@@ -105,15 +149,21 @@ contract Bachvanity {
         order.currentPoints = points;
         order.bestSalt = salt;
         order.bestSubmitter = msg.sender;
+        emit SaltSubmitted(orderId, msg.sender, salt, points);
         if(points == order.maxPoints) {
             // perfect match, payout immediately
             uint256 amount = order.amount;
             order.amount = 0;
             (bool success, ) = msg.sender.call{value: amount}("");
             require(success, "!transfer");
+            emit OrderClosed(orderId, msg.sender, amount);
         }
     }
 
+    /*
+    * @dev claim reward after deadline (winner only)
+    * @param orderId id of the order
+    */
     function claimReward(uint256 orderId) public {
         Orders storage order = orders[orderId];
         require(block.timestamp > order.deadline, "!deadline");
@@ -123,8 +173,13 @@ contract Bachvanity {
         order.amount = 0;
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "!transfer");
+        emit OrderClosed(orderId, msg.sender, amount);
     }
 
+    /*
+    * @dev claim back funds after deadline (if no winner)
+    * @param orderId id of the order
+    */
     function claimDeadOrder(uint256 orderId) public {
         Orders storage order = orders[orderId];
         require(block.timestamp > order.deadline, "!deadline");
@@ -135,20 +190,22 @@ contract Bachvanity {
         order.amount = 0;
         (bool success, ) = msg.sender.call{value: amount}("");
         require(success, "!transfer");
+        emit OrderClosed(orderId, address(0), 0);
     }
 
-    function simPoints(uint256 orderId, bytes32 salt) public view returns(uint8) {
-        Orders memory order = orders[orderId];
-        bytes32 bytecodeHash = order.bytecodeHash;
-        address deployer = order.deployer;
-        address addr = computeAddress(salt, bytecodeHash, deployer);
-        bytes memory addrBytes = bytes(Strings.toHexString(uint256(uint160(addr)), 20));
-        uint8 points = 0;
-        for(uint256 i = 2; i < addrBytes.length; i++) {
-            if(order.desired[i - 2] == addrBytes[i]) {
-                points++;
+    /*
+    * @dev deploy contract using create2
+    * @param code contract bytecode
+    * @param salt salt used for create2
+    */
+    function deploy(bytes memory code, bytes32 salt) public {
+        address addr;
+        assembly {
+            addr := create2(0, add(code, 0x20), mload(code), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
             }
         }
-        return points;
+        emit Deployed(addr, salt);
     }
 }
